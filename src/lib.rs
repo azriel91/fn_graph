8e9e92262969cf6400f39cbe90cd2008fbdbab1f
Azriel Hoh <azriel91@gmail.com>
2022-05-26 07:47:50 +1200

    Update `README.md` and docs.

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,10 @@
 //! ```toml
 //! fn_graph = "0.4.0"
 //!
-//! # Integrate with `fn_meta`
+//! # Integrate with `fn_meta` and/or `resman`
 //! fn_graph = { version = "0.4.0", features = ["fn_meta"] }
+//! fn_graph = { version = "0.4.0", features = ["resman"] }
+//! fn_graph = { version = "0.4.0", features = ["fn_meta", "resman"] }
 //! ```
 //!
 //! # Rationale
