ec7c5f230137ae15a380df84883631c678eb4306
Azriel Hoh <azriel91@gmail.com>
2023-01-18 10:57:27 +1300

    Add tests for `FnIdInner` and `Rank`.

--- src/rank.rs
+++ src/rank.rs
@@ -19,3 +19,40 @@ impl Add<usize> for Rank {
         Self(self.0 + other)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{cmp::Ordering, collections::HashMap};
+
+    use super::Rank;
+
+    #[test]
+    fn debug() {
+        assert_eq!("Rank(1)", format!("{:?}", Rank(1)));
+    }
+
+    #[test]
+    fn add() {
+        assert_eq!(Rank(3), Rank(1) + Rank(2));
+    }
+
+    #[test]
+    fn add_usize() {
+        assert_eq!(Rank(3), Rank(1) + 2);
+    }
+
+    #[test]
+    fn hash() {
+        let mut map = HashMap::new();
+        map.insert(Rank(1), 1u32);
+
+        assert_eq!(Some(1u32), map.get(&Rank(1)).copied());
+    }
+
+    #[test]
+    fn partial_ord() {
+        assert!(Rank(1) > Rank(0));
+        assert!(Rank(0) < Rank(1));
+        assert_eq!(Ordering::Equal, Rank(1).cmp(&Rank(1)));
+    }
+}
