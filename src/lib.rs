4825dd2205ce2b123d2801638678e2f74633acee
Azriel Hoh <azriel91@gmail.com>
2025-04-18 11:32:15 +1200

    Update crate version to `0.18.0`.

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,13 @@
 //! Add the following to `Cargo.toml`
 //!
 //! ```toml
-//! fn_graph = "0.17.0"
+//! fn_graph = "0.18.0"
 //!
 //! # Integrate with `fn_meta` / `interruptible` / `resman`
-//! fn_graph = { version = "0.17.0", features = ["fn_meta"] }
-//! fn_graph = { version = "0.17.0", features = ["interruptible"] }
-//! fn_graph = { version = "0.17.0", features = ["resman"] }
-//! fn_graph = { version = "0.17.0", features = ["fn_meta", "interruptible", "resman"] }
+//! fn_graph = { version = "0.18.0", features = ["fn_meta"] }
+//! fn_graph = { version = "0.18.0", features = ["interruptible"] }
+//! fn_graph = { version = "0.18.0", features = ["resman"] }
+//! fn_graph = { version = "0.18.0", features = ["fn_meta", "interruptible", "resman"] }
 //! ```
 //!
 //! # Rationale
